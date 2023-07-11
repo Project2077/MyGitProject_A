8b32b0c5e0ac229b9df9ade7b763f02106fa7b02
Y Y <Wryyyyyyyyyy@protonmail.com>
2023-07-12 00:32:20 +0800

    First add with VS

---
+++ PythonSample.py
@@ -0,0 +1,7 @@
+print("------------------------------")
+print("Python has entered the building.")
+print("Let the party start!")
+print("------------------------------")
+for cup in range(0,10):
+    print("I'm drinking cup " + str(cup))
+
---
+++ jsSample.js
@@ -0,0 +1,10 @@
+console.log("------------!!!!");
+console.log("Rise & Shine! !!!");
+console.log("Ready for a new day!");
+console.log("-------------");
+console.log("New Feathers Branch!");
+console.log("-------------");
+
+
+console.log("Add 2");
+console.log("Add 3");
