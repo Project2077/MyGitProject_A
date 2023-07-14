59d9008fad4f8ad99ff947c542e96b347135cc1c
Y Y <Wryyyyyyyyyy@protonmail.com>
2023-07-14 23:28:30 +0800

    deleted a sys version

--- PythonSample.py
+++ PythonSample.py
@@ -1,12 +1,6 @@
-import sys
-
-
 print("------------------------------")
 print("Python has entered the building.")
 print("Let the party start!")
-
-print(sys.version)
-
 print("------------------------------")
 for cup in range(0,10):
     print("I'm drinking cup " + str(cup))
